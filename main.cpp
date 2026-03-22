e2e6576c3a09ceaed5c177a973fb5e2f30797329
ZigaPesti <ziga.pesti@student.um.si>
2026-03-22 18:15:41 +0100

    Added main logic

--- main.cpp
+++ main.cpp
@@ -10,3 +10,25 @@ void binaryRadixSort(vector<unsigned char>& arr) {
         countingSortByBit(arr, k);
     }
 }
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        cerr << "Uporaba: " << argv[0] << " <vhodna_datoteka>\n";
+        return 1;
+    }
+    ifstream inFile(argv[1]);
+    if (!inFile) {
+        cerr << "Napaka pri odpiranju vhodne datoteke.\n";
+        return 1;
+    }
+    
+    vector<unsigned char> arr;
+    int temp;
+    while (inFile >> temp) {
+        arr.push_back(static_cast<unsigned char>(temp));
+    }
+    inFile.close();
+
+    binaryRadixSort(arr);
+    return 0;
+}
